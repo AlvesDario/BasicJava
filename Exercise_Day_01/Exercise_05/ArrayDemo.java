6ce1d2a840093944d2f3fef61133b4849a5ce907
Dario Alves <dario.alves@fatec.sp.gov.br>
2020-04-02 15:02:32 +0000

    Exercise 05 done

---
+++ Exercise_Day_01/Exercise_05/ArrayDemo.java
@@ -0,0 +1,13 @@
+package Exercise_Day_01.Exercise_05;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String[][] animalArray = {{"Cat", "Meoow"}, {"Dog", "Ruff Ruff"}};
+
+        int option = 0;
+
+        System.out.println("the " + animalArray[option][0] 
+                            + " does " + animalArray[option][1] + " !!!");
+
+    }
+}
